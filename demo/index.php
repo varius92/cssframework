4d95d1c8e8a0aaf04246663b634e9aaf3a47d10a
Andreas Seifert <a.seifert@makrohaus.de>
2017-03-07 10:31:38 +0100

    Demo angelegt

---
+++ demo/index.php
@@ -0,0 +1,176 @@
+<!doctype html>
+<html lang="de">
+<head>
+  <meta charset="UTF-8">
+  <title>Demo</title>
+  <link rel="stylesheet" href="../less/main.css">
+  <link rel="stylesheet" href="less/demo.css">
+</head>
+<body>
+
+
+<section class="section section--dark">
+  <div class="grid grid--container">
+    <div class="row">
+      <div class="col col-md-12">
+        <div class="section__header section--dark__header"><h2 class="section__title section--dark__title">Individual
+            sizing</h2><span class="section__underline section--dark__underline"></span>
+          <small class="section__subtitle section--dark__subtitle">It is possible to size the columns individually
+          </small>
+        </div>
+        <div class="section__body section--dark__body">
+          <div class="row">
+            <div class="col col--sm-12">
+              <div class="block block--dark">
+                <div class="block__content block--dark__content"></div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col col--sm-11">
+              <div class="block block--dark">
+                <div class="block--dark__content"></div>
+              </div>
+            </div>
+            <div class="col col--sm-1">
+              <div class="block block--dark">
+                <div class="block--dark__content"></div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col col--sm-10">
+              <div class="block block--dark">
+                <div class="block--dark__content"></div>
+              </div>
+            </div>
+            <div class="col col--sm-2">
+              <div class="block block--dark">
+                <div class="block--dark__content"></div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col col--sm-9">
+              <div class="block block--dark">
+                <div class="block--dark__content"></div>
+              </div>
+            </div>
+            <div class="col col--sm-3">
+              <div class="block block--dark">
+                <div class="block--dark__content"></div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col col--sm-8">
+              <div class="block block--dark">
+                <div class="block--dark__content"></div>
+              </div>
+            </div>
+            <div class="col col--sm-4">
+              <div class="block block--dark">
+                <div class="block--dark__content"></div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col col--sm-7">
+              <div class="block block--dark">
+                <div class="block--dark__content"></div>
+              </div>
+            </div>
+            <div class="col col--sm-5">
+              <div class="block block--dark">
+                <div class="block--dark__content"></div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col col--sm-6">
+              <div class="block block--dark">
+                <div class="block--dark__content"></div>
+              </div>
+            </div>
+            <div class="col col--sm-6">
+              <div class="block block--dark">
+                <div class="block--dark__content"></div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col col--sm-5">
+              <div class="block block--dark">
+                <div class="block--dark__content"></div>
+              </div>
+            </div>
+            <div class="col col--sm-7">
+              <div class="block block--dark">
+                <div class="block--dark__content"></div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col col--sm-4">
+              <div class="block block--dark">
+                <div class="block--dark__content"></div>
+              </div>
+            </div>
+            <div class="col col--sm-8">
+              <div class="block block--dark">
+                <div class="block--dark__content"></div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col col--sm-3">
+              <div class="block block--dark">
+                <div class="block--dark__content"></div>
+              </div>
+            </div>
+            <div class="col col--sm-9">
+              <div class="block block--dark">
+                <div class="block--dark__content"></div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col col--sm-2">
+              <div class="block block--dark">
+                <div class="block--dark__content"></div>
+              </div>
+            </div>
+            <div class="col col--sm-10">
+              <div class="block block--dark">
+                <div class="block--dark__content"></div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col col--sm-1">
+              <div class="block block--dark">
+                <div class="block--dark__content"></div>
+              </div>
+            </div>
+            <div class="col col--sm-11">
+              <div class="block block--dark">
+                <div class="block--dark__content"></div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col col--sm-12">
+              <div class="block block--dark">
+                <div class="block--dark__content"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+</body>
+</html>
