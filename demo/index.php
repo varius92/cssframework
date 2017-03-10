112f3f3c63f2a5b238b370d986b837a79db84ed2
Andreas Seifert <a.seifert@makrohaus.de>
2017-03-10 09:18:53 +0100

    Typography ergänzt; Headlines Mixin;

--- demo/index.php
+++ demo/index.php
@@ -3,164 +3,311 @@
 <head>
   <meta charset="UTF-8">
   <title>Demo</title>
-  <link rel="stylesheet" href="../less/main.css">
   <link rel="stylesheet" href="less/demo.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
+<div class="grid grid--container">
+  <div class="row">
+    <div class="col">
+      <h1>Typo</h1>
+      <p class="lead">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, amet animi, beatae delectus
+        deleniti
+        doloremque error eveniet ex explicabo illo labore laborum neque nisi numquam obcaecati odio, optio praesentium
+        quae.
+      </p>
+      <p class="zeitung">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+        consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+        diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+        takimata sanctus est Lorem ipsum dolor sit amet.
+      </p>
+      <h1>Headline 1</h1>
+      <h2>Headline 2</h2>
+      <h3>Headline 3</h3>
+      <h4>Headline 4</h4>
+<!--      <h5>Headline 5</h5>-->
+<!--      <h6>Headline 6</h6>-->
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, amet animi, beatae delectus
+        deleniti
+        doloremque error eveniet ex explicabo illo labore laborum neque nisi numquam obcaecati odio, optio praesentium
+        quae.
+        Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu
+        feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril
+        delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+        sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+      </p>
+      <blockquote>
+        Blockqoute: For 50 years, WWF has been protecting the future of nature. The world's leading
+        conservation organization, WWF works in 100 countries and is supported by 1.2 million members in the United
+        States and close to 5 million globally.
+      </blockquote>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+        consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+        diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+        takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+        diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+        accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+        dolor sit amet.
+        Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea
+        commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel
+        illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent
+        luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+      </p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias asperiores cupiditate dolor earum esse id magnam
+        nesciunt officia officiis pariatur praesentium quibusdam quisquam quo repellendus, similique suscipit tempora
+        velit veniam?
+      </p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col col--md-6">
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+        consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+        diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+        takimata sanctus est Lorem ipsum dolor sit amet.
+      </p>
+    </div>
+    <div class="col col--md-6">
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+        consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+        diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+        takimata sanctus est Lorem ipsum dolor sit amet.
+      </p>
+    </div>
+  </div>
 
-
-<section class="section section--dark">
+  <div class="row">
+    <div class="col col--md-4">
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+        consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+        diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+        takimata sanctus est Lorem ipsum dolor sit amet.
+      </p>
+    </div>
+    <div class="col col--md-4">
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+        consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+        diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+        takimata sanctus est Lorem ipsum dolor sit amet.
+      </p>
+    </div>
+    <div class="col col--md-4">
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+        consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+        diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+        takimata sanctus est Lorem ipsum dolor sit amet.
+      </p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col col--md-3">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aut consectetur eius est hic inventore ipsam minus
+        modi odit perspiciatis quas quod ratione repellendus similique sit, vel veniam voluptatem voluptates.
+      </p>
+    </div>
+    <div class="col col--md-3">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aut consectetur eius est hic inventore ipsam minus
+        modi odit perspiciatis quas quod ratione repellendus similique sit, vel veniam voluptatem voluptates.
+      </p>
+    </div>
+    <div class="col col--md-3">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aut consectetur eius est hic inventore ipsam minus
+        modi odit perspiciatis quas quod ratione repellendus similique sit, vel veniam voluptatem voluptates.
+      </p>
+    </div>
+    <div class="col col--md-3">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aut consectetur eius est hic inventore ipsam minus
+        modi odit perspiciatis quas quod ratione repellendus similique sit, vel veniam voluptatem voluptates.
+      </p>
+    </div>
+  </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<section>
   <div class="grid grid--container">
     <div class="row">
       <div class="col col-md-12">
-        <div class="section__header section--dark__header"><h2 class="section__title section--dark__title">Individual
-            sizing</h2><span class="section__underline section--dark__underline"></span>
-          <small class="section__subtitle section--dark__subtitle">It is possible to size the columns individually
-          </small>
-        </div>
         <div class="section__body section--dark__body">
           <div class="row">
             <div class="col col--sm-12">
-              <div class="block block--dark">
-                <div class="block__content block--dark__content"></div>
+              <div class="block">
+                <div class="block__content__content"></div>
               </div>
             </div>
           </div>
           <div class="row">
             <div class="col col--sm-11">
-              <div class="block block--dark">
+              <div class="block">
                 <div class="block--dark__content"></div>
               </div>
             </div>
             <div class="col col--sm-1">
-              <div class="block block--dark">
+              <div class="block">
                 <div class="block--dark__content"></div>
               </div>
             </div>
           </div>
           <div class="row">
             <div class="col col--sm-10">
-              <div class="block block--dark">
+              <div class="block">
                 <div class="block--dark__content"></div>
               </div>
             </div>
             <div class="col col--sm-2">
-              <div class="block block--dark">
+              <div class="block">
                 <div class="block--dark__content"></div>
               </div>
             </div>
           </div>
           <div class="row">
             <div class="col col--sm-9">
-              <div class="block block--dark">
+              <div class="block">
                 <div class="block--dark__content"></div>
               </div>
             </div>
             <div class="col col--sm-3">
-              <div class="block block--dark">
+              <div class="block">
                 <div class="block--dark__content"></div>
               </div>
             </div>
           </div>
           <div class="row">
             <div class="col col--sm-8">
-              <div class="block block--dark">
+              <div class="block">
                 <div class="block--dark__content"></div>
               </div>
             </div>
             <div class="col col--sm-4">
-              <div class="block block--dark">
+              <div class="block">
                 <div class="block--dark__content"></div>
               </div>
             </div>
           </div>
           <div class="row">
             <div class="col col--sm-7">
-              <div class="block block--dark">
+              <div class="block">
                 <div class="block--dark__content"></div>
               </div>
             </div>
             <div class="col col--sm-5">
-              <div class="block block--dark">
+              <div class="block">
                 <div class="block--dark__content"></div>
               </div>
             </div>
           </div>
           <div class="row">
             <div class="col col--sm-6">
-              <div class="block block--dark">
+              <div class="block">
                 <div class="block--dark__content"></div>
               </div>
             </div>
             <div class="col col--sm-6">
-              <div class="block block--dark">
+              <div class="block">
                 <div class="block--dark__content"></div>
               </div>
             </div>
           </div>
           <div class="row">
             <div class="col col--sm-5">
-              <div class="block block--dark">
+              <div class="block">
                 <div class="block--dark__content"></div>
               </div>
             </div>
             <div class="col col--sm-7">
-              <div class="block block--dark">
+              <div class="block">
                 <div class="block--dark__content"></div>
               </div>
             </div>
           </div>
           <div class="row">
             <div class="col col--sm-4">
-              <div class="block block--dark">
+              <div class="block">
                 <div class="block--dark__content"></div>
               </div>
             </div>
             <div class="col col--sm-8">
-              <div class="block block--dark">
+              <div class="block">
                 <div class="block--dark__content"></div>
               </div>
             </div>
           </div>
           <div class="row">
             <div class="col col--sm-3">
-              <div class="block block--dark">
+              <div class="block">
                 <div class="block--dark__content"></div>
               </div>
             </div>
             <div class="col col--sm-9">
-              <div class="block block--dark">
+              <div class="block">
                 <div class="block--dark__content"></div>
               </div>
             </div>
           </div>
           <div class="row">
             <div class="col col--sm-2">
-              <div class="block block--dark">
+              <div class="block">
                 <div class="block--dark__content"></div>
               </div>
             </div>
             <div class="col col--sm-10">
-              <div class="block block--dark">
+              <div class="block">
                 <div class="block--dark__content"></div>
               </div>
             </div>
           </div>
           <div class="row">
             <div class="col col--sm-1">
-              <div class="block block--dark">
+              <div class="block">
                 <div class="block--dark__content"></div>
               </div>
             </div>
             <div class="col col--sm-11">
-              <div class="block block--dark">
+              <div class="block">
                 <div class="block--dark__content"></div>
               </div>
             </div>
           </div>
           <div class="row">
             <div class="col col--sm-12">
-              <div class="block block--dark">
+              <div class="block">
                 <div class="block--dark__content"></div>
               </div>
             </div>
@@ -171,6 +318,18 @@
   </div>
 </section>
 
-
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
 </body>
 </html>
